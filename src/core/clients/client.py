e76ddd2c4b51210b11afe24e1d7bf5878427a3b0
Roeia Amr <roeiaamr99@gmail.com>
2023-03-24 17:37:52 +0200

    added testing model and testing dataset

--- src/core/clients/client.py
+++ src/core/clients/client.py
@@ -1,19 +1,37 @@
-import torch
 from collections import OrderedDict
 import flwr as fl
-import numpy as np
+from src.core.model.testing_model import Net
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+
+def load_data():
+    """Load CIFAR-10 (training and test set)."""
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
+    trainset = CIFAR10(".", train=True, download=True, transform=transform)
+    testset = CIFAR10(".", train=False, download=True, transform=transform)
+    trainloader = DataLoader(trainset, batch_size=32, shuffle=True)
+    testloader = DataLoader(testset, batch_size=32)
+    num_examples = {"trainset": len(trainset), "testset": len(testset)}
+    return trainloader, testloader, num_examples
+
+
 class NCFClient(fl.client.NumPyClient):
-    def __init__(self, model, train_data, val_data, optimizer, batch_size, learning_rate):
+    def __init__(self, model, train_data, val_data):
         self.model = model
         self.train_data = train_data
         self.val_data = val_data
-        self.optimizer = optimizer
-        self.batch_size = batch_size
-        self.learning_rate = learning_rate
+        self.batch_size = 32
+        self.learning_rate = 0.001
 
     def train(self, trainloader, epochs):
         """Train the model on the training set."""
@@ -26,6 +44,21 @@ class NCFClient(fl.client.NumPyClient):
                 loss = criterion(self.model(images), labels)
                 loss.backward()
                 optimizer.step()
+
+    def test(self, testloader):
+        """Validate the network on the entire test set."""
+        criterion = torch.nn.CrossEntropyLoss()
+        correct, total, loss = 0, 0, 0.0
+        with torch.no_grad():
+            for data in testloader:
+                images, labels = data[0].to(DEVICE), data[1].to(DEVICE)
+                outputs = self.model(images)
+                loss += criterion(outputs, labels).item()
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        accuracy = correct / total
+        return loss, accuracy
 
     def get_parameters(self, config):
         return [val.cpu().numpy() for _, val in self.model.state_dict().items()]
@@ -42,5 +75,14 @@ class NCFClient(fl.client.NumPyClient):
 
     def evaluate(self, parameters, config):
         self.set_parameters(parameters)
-        loss, accuracy = test(self.model, testloader)
+        # TODO : Change to Get the Hit Ratio and NDCG
+        loss, accuracy = self.test(self.model, testloader)
         return float(loss), num_examples["testset"], {"accuracy": float(accuracy)}
+
+
+if __name__ == '__main__':
+    # Load model and data
+    net = Net().to(DEVICE)
+    trainloader, testloader, num_examples = load_data()
+
+    fl.client.start_numpy_client(server_address="[::]:8080", client=NCFClient(model=net, train_data=trainloader, val_data=testloader))
