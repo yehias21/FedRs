573af29b45e306da274513c8fae2a39cdc0e3d11
Omar Mokhtar <omokhtarm@gmail.com>
2023-03-28 07:12:13 +0200

    added plotting using tensorboard

--- src/core/clients/client.py
+++ src/core/clients/client.py
@@ -1,11 +1,14 @@
+import argparse
 from collections import OrderedDict
+from datetime import datetime
 
 import flwr as fl
 import torch
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 from torchvision.datasets import CIFAR10
-from tqdm import tqdm
+from tqdm.auto import tqdm
 
 from src.core.model.testing_model import Net
 
@@ -17,8 +20,8 @@ def load_data():
     transform = transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
     )
-    trainset = CIFAR10(".", train=True, download=True, transform=transform)
-    testset = CIFAR10(".", train=False, download=True, transform=transform)
+    trainset = CIFAR10(root="./src/core/clients/", train=True, download=True, transform=transform)
+    testset = CIFAR10(root="./src/core/clients/", train=False, download=True, transform=transform)
     trainloader = DataLoader(trainset, batch_size=32, shuffle=True)
     testloader = DataLoader(testset, batch_size=32)
     num_examples = {"trainset": len(trainset), "testset": len(testset)}
@@ -26,32 +29,53 @@ def load_data():
 
 
 class NCFClient(fl.client.NumPyClient):
-    def __init__(self, model, trainloader, testloader, num_examples):
+    def __init__(self,
+                 cid: int,
+                 model,
+                 trainloader: DataLoader,
+                 testloader: DataLoader,
+                 num_examples,
+                 log: bool = False
+                 ):
+        self.cid = cid
+        self.writer = SummaryWriter(log_dir=f"runs/{datetime.now():%Y-%m-%d_%H:%M}/Client{self.cid}")
+        print('Starting Client', self.cid)
         self.model = model
         self.trainloader = trainloader
         self.testloader = testloader
         self.batch_size = 32
         self.learning_rate = 0.001
         self.num_examples = num_examples
+        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.learning_rate, momentum=0.9)
+        self.log = log
 
-    def train(self, epochs):
+    def train(self, epochs, server_round):
         """Train the model on the training set."""
         criterion = torch.nn.CrossEntropyLoss()
-        optimizer = torch.optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9)
-        for _ in range(epochs):
-            for images, labels in tqdm(self.trainloader):
+        n_total_steps = len(self.trainloader)
+        for epoch in range(epochs):
+            running_loss = 0.0
+            for i, (images, labels) in enumerate(pbar := tqdm(self.trainloader)):
                 images, labels = images.to(DEVICE), labels.to(DEVICE)
-                optimizer.zero_grad()
+                self.optimizer.zero_grad()
                 loss = criterion(self.model(images), labels)
                 loss.backward()
-                optimizer.step()
+                self.optimizer.step()
+                running_loss += loss.item()
+                pbar.set_description(f"Round[{server_round}], Client[{self.cid}], Epoch [{epoch + 1}/{epochs}]")
+                if (i + 1) % 100 == 0:
+                    if self.log:
+                        self.writer.add_scalar("running_loss",
+                                               running_loss / 100,
+                                               (server_round - 1) * (epoch + 1) * n_total_steps + i)
+                    running_loss = 0.0
 
     def test(self):
         """Test the network on the entire test set."""
         criterion = torch.nn.CrossEntropyLoss()
         correct, total, loss = 0, 0, 0.0
         with torch.no_grad():
-            for data in tqdm(self.testloader):
+            for data in tqdm(self.testloader, desc=f"Client[{self.cid}] Testing .. "):
                 images, labels = data[0].to(DEVICE), data[1].to(DEVICE)
                 outputs = self.model(images)
                 loss += criterion(outputs, labels).item()
@@ -71,25 +95,33 @@ class NCFClient(fl.client.NumPyClient):
 
     def fit(self, parameters, config):
         self.set_parameters(parameters)
-        self.train(epochs=1)
+        self.train(epochs=config['local_epochs'], server_round=config['server_round'])
         return self.get_parameters(config={}), self.num_examples["trainset"], {}
 
     def evaluate(self, parameters, config):
         self.set_parameters(parameters)
         # TODO : Change to Get the Hit Ratio and NDCG
         loss, accuracy = self.test()
-        return float(loss), self.num_examples["testset"], {"accuracy": float(accuracy)}
+        return float(loss), self.num_examples["testset"], {"accuracy": float(accuracy),
+                                                           "loss": float(loss),
+                                                           "server_round": config["server_round"]}
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cid', type=int, required=True)
+    parser.add_argument('--log', type=bool, required=False, default=False)
+    args = parser.parse_args()
     # Load model and data
     net = Net().to(DEVICE)
     trainloader, testloader, num_examples = load_data()
 
     fl.client.start_numpy_client(server_address="localhost:8080",
-                                 client=NCFClient(model=net,
+                                 client=NCFClient(cid=args.cid,
+                                                  model=net,
                                                   trainloader=trainloader,
                                                   testloader=testloader,
-                                                  num_examples=num_examples
+                                                  num_examples=num_examples,
+                                                  log=args.log
                                                   )
                                  )
--- src/core/servers/server.py
+++ src/core/servers/server.py
@@ -1,25 +1,62 @@
+from datetime import datetime
 from typing import List, Tuple
 
 import flwr as fl
 from flwr.common import Metrics
+from torch.utils.tensorboard import SummaryWriter
+
+SERVER_WRITER = SummaryWriter(log_dir=f"runs/{datetime.now():%Y-%m-%d_%H:%M}/Server")
 
 
 # Define metric aggregation function
 def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
+    server_round = metrics[0][1]["server_round"]
     # Multiply accuracy of each client by number of examples used
     accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
+    losses = [num_examples * m["loss"] for num_examples, m in metrics]
     examples = [num_examples for num_examples, _ in metrics]
 
     # Aggregate and return custom metric (weighted average)
-    return {"accuracy": sum(accuracies) / sum(examples)}
+    round_accuracy = sum(accuracies) / sum(examples)
+    round_loss = sum(losses) / sum(examples)
+
+    SERVER_WRITER.add_scalar('loss', round_loss, server_round)
+    SERVER_WRITER.add_scalar('accuracy', round_accuracy, server_round)
+
+    return {"accuracy": round_accuracy}
 
 
-# Define strategy
-strategy = fl.server.strategy.FedAvg(evaluate_metrics_aggregation_fn=weighted_average)
+def fit_config(server_round: int):
+    """Return training configuration dict for each round.
 
-# Start Flower server
-fl.server.start_server(
-    server_address="localhost:8080",
-    config=fl.server.ServerConfig(num_rounds=3),
-    strategy=strategy,
-)
+    Perform two rounds of training with one local epoch, increase to two local
+    epochs afterwards.
+    """
+    config = {
+        "server_round": server_round,  # The current round of federated learning
+        "local_epochs": 1,
+    }
+    return config
+
+
+def eval_config(server_round: int):
+    """Return evaluation configuration dict for each round.
+    """
+    config = {
+        "server_round": server_round,  # The current round of federated learning
+    }
+    return config
+
+
+if __name__ == '__main__':
+    # Define strategy
+    strategy = fl.server.strategy.FedAvg(evaluate_metrics_aggregation_fn=weighted_average,
+                                         on_fit_config_fn=fit_config,
+                                         on_evaluate_config_fn=eval_config,
+                                         )
+    # Start Flower server
+    fl.server.start_server(
+        server_address="localhost:8080",
+        config=fl.server.ServerConfig(num_rounds=3),
+        strategy=strategy,
+    )
