e18b26ae1476508f37b3b85ad3ca6dcf85d9eb99
Yahia Salaheldin Shaaban <yehia.salah.ms@alexu.edu.eg>
2023-04-01 15:28:45 +0200

    model

--- src/core/model/model.py
+++ src/core/model/model.py
@@ -1,81 +1,19 @@
 import torch
 import torch.nn as nn
 
-
-class Generalized_Matrix_Factorization(nn.Module):
-    def __init__(self, args, num_users, num_items):
-        super(Generalized_Matrix_Factorization, self).__init__()
-        self.num_users = num_users
-        self.num_items = num_items
-        self.factor_num = args.factor_num
-
-        self.embedding_user = nn.Embedding(num_embeddings=self.num_users, embedding_dim=self.factor_num)
-        self.embedding_item = nn.Embedding(num_embeddings=self.num_items, embedding_dim=self.factor_num)
-
-        self.affine_output = nn.Linear(in_features=self.factor_num, out_features=1)
-        self.logistic = nn.Sigmoid()
-
-    def forward(self, user_indices, item_indices):
-        user_embedding = self.embedding_user(user_indices)
-        item_embedding = self.embedding_item(item_indices)
-        element_product = torch.mul(user_embedding, item_embedding)
-        logits = self.affine_output(element_product)
-        rating = self.logistic(logits)
-        return rating
-
-    def init_weight(self):
-        pass
-
-
-class Multi_Layer_Perceptron(nn.Module):
-    def __init__(self, args, num_users, num_items):
-        super(Multi_Layer_Perceptron, self).__init__()
-        self.num_users = num_users
-        self.num_items = num_items
-        self.factor_num = args.factor_num
-        self.layers = args.layers
-
-        self.embedding_user = nn.Embedding(num_embeddings=self.num_users, embedding_dim=self.factor_num)
-        self.embedding_item = nn.Embedding(num_embeddings=self.num_items, embedding_dim=self.factor_num)
-
-        self.fc_layers = nn.ModuleList()
-        for idx, (in_size, out_size) in enumerate(zip(self.layers[:-1], self.layers[1:])):
-            self.fc_layers.append(nn.Linear(in_size, out_size))
-
-        self.affine_output = nn.Linear(in_features=self.layers[-1], out_features=1)
-        self.logistic = nn.Sigmoid()
-
-    def forward(self, user_indices, item_indices):
-        user_embedding = self.embedding_user(user_indices)
-        item_embedding = self.embedding_item(item_indices)
-        vector = torch.cat([user_embedding, item_embedding], dim=-1)  # the concat latent vector
-        for idx, _ in enumerate(range(len(self.fc_layers))):
-            vector = self.fc_layers[idx](vector)
-            vector = nn.ReLU()(vector)
-            # vector = nn.BatchNorm1d()(vector)
-            # vector = nn.Dropout(p=0.5)(vector)
-        logits = self.affine_output(vector)
-        rating = self.logistic(logits)
-        return rating
-
-    def init_weight(self):
-        pass
-
-
 class NeuMF(nn.Module):
-    def __init__(self, args, num_users, num_items):
+    def __init__(self, args, num_items):
         super(NeuMF, self).__init__()
-        self.num_users = num_users
         self.num_items = num_items
         self.factor_num_mf = args.factor_num
         self.factor_num_mlp = int(args.layers[0] / 2)
         self.layers = args.layers
         self.dropout = args.dropout
 
-        self.embedding_user_mlp = nn.Embedding(num_embeddings=self.num_users, embedding_dim=self.factor_num_mlp)
+        self.embedding_user_mlp = nn.Embedding(num_embeddings=1, embedding_dim=self.factor_num_mlp)
         self.embedding_item_mlp = nn.Embedding(num_embeddings=self.num_items, embedding_dim=self.factor_num_mlp)
 
-        self.embedding_user_mf = nn.Embedding(num_embeddings=self.num_users, embedding_dim=self.factor_num_mf)
+        self.embedding_user_mf = nn.Embedding(num_embeddings=1, embedding_dim=self.factor_num_mf)
         self.embedding_item_mf = nn.Embedding(num_embeddings=self.num_items, embedding_dim=self.factor_num_mf)
 
         self.fc_layers = nn.ModuleList()
@@ -103,19 +41,13 @@ class NeuMF(nn.Module):
             if isinstance(m, nn.Linear) and m.bias is not None:
                 m.bias.data.zero_()
 
-    def forward(self, user_indices, item_indices):
-        user_embedding_mlp = self.embedding_user_mlp(user_indices)
+    def forward(self,item_indices):
         item_embedding_mlp = self.embedding_item_mlp(item_indices)
-
-        user_embedding_mf = self.embedding_user_mf(user_indices)
         item_embedding_mf = self.embedding_item_mf(item_indices)
-
-        mlp_vector = torch.cat([user_embedding_mlp, item_embedding_mlp], dim=-1)  # the concat latent vector
-        mf_vector = torch.mul(user_embedding_mf, item_embedding_mf)
-
+        mlp_vector = torch.cat([self.embedding_user_mlp, item_embedding_mlp], dim=-1)  # the concat latent vector
+        mf_vector = torch.mul(self.embedding_user_mf, item_embedding_mf)
         for idx, _ in enumerate(range(len(self.fc_layers))):
             mlp_vector = self.fc_layers[idx](mlp_vector)
-
         vector = torch.cat([mlp_vector, mf_vector], dim=-1)
         logits = self.affine_output(vector)
         rating = self.logistic(logits)
